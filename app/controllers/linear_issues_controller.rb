a3b5dcc2b1123f47879dccbc8128aae80f80142f
Kai <hulloitskai@gmail.com>
2023-01-09 14:11:44 -0500

    Nicer error messages when creating a Linear issue

--- app/controllers/linear_issues_controller.rb
+++ app/controllers/linear_issues_controller.rb
@@ -9,9 +9,37 @@ class LinearIssuesController < ApplicationController
   # POST /issues
   sig { void }
   def create
-    issue = Linear.create_issue(**T.unsafe(issue_params))
+    issue_params = self.issue_params
+    title = issue_params[:title] or raise "Missing title"
+    title = T.cast(title, String)
+    description = issue_params[:description]
+    description = T.cast(description, T.nilable(String))
+    issue = Linear.create_issue(title:, description:)
     issue_payload = issue.to_h.slice("id", "title", "description")
-    render(json: issue_payload)
+  rescue => error
+    respond_to do |format|
+      format.html { raise }
+      format.text do
+        render(
+          status: :internal_server_error,
+          plain: "Failed to add issue: #{error.message}",
+        )
+      end
+      format.json do
+        render(
+          status: :internal_server_error,
+          json: { error: error.message },
+        )
+      end
+    end
+  else
+    respond_to do |format|
+      format.html do
+        redirect_to(root_path, notice: "Issue added successfully!")
+      end
+      format.text { render(plain: "Issue added successfully!") }
+      format.json { render(json: issue_payload) }
+    end
   end
 
   private
