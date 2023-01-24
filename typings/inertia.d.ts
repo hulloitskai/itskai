de93fade4671f130ef4930b15e82f0c47511103b
Kai <hulloitskai@gmail.com>
2023-01-24 18:23:40 -0500

    Fix SSR

--- typings/inertia.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@inertiajs/react/server" {
-  export default function createServer(
-    render: (page: Page<unknown>) => Promise<{
-      head?: string[];
-      body: string;
-    }>,
-  ): void;
-}
