30161bc34b3651dfbe2b310802dce2c2262dfa98
Kai <hulloitskai@gmail.com>
2023-06-25 01:10:53 -0400

    Raise error if missing default from parameter is set in mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -7,10 +7,29 @@ class ApplicationMailer < ActionMailer::Base
   include Querying
 
   # == Configuration
-  default from: ENV["RAILS_MAILER_FROM"], reply_to: ENV["RAILS_MAILER_REPLY_TO"]
+  default from: :default_from, reply_to: ENV["RAILS_MAILER_REPLY_TO"]
   layout "mailer"
 
   # == Querying
   sig { override.returns(T.nilable(T.any(User, Symbol))) }
   def current_user = :system
+
+  protected
+
+  # == Helpers
+  sig { returns(String) }
+  def default_from
+    return T.must(@default_from) if defined?(@default_from)
+    @default_from = T.let(@default_from, T.nilable(String))
+    @default_from = ENV["RAILS_MAILER_FROM"] or
+      raise "Missing default from address"
+  end
+
+  private
+
+  # == Helpers
+  sig { override.params(value: T.untyped).returns(T.untyped) }
+  def compute_default(value)
+    value.is_a?(Symbol) ? send(value) : super
+  end
 end
