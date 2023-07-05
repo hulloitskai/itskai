7add6b7fd261d209fad6d310dfa89f2df49851b6
Kai <hulloitskai@gmail.com>
2023-07-05 09:21:16 -0400

    Configure rubocop and eslint to cache in tmp/cache

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ export default {
   "*.{js,mjs,cjs,jsx,ts,cts,mts,tsx,css,md,html,json,yaml,yml}":
     "prettier --list-different",
   "*.{js,mjs,cjs,jsx,ts,cts,mts,tsx,graphql}":
-    "eslint --report-unused-disable-directives",
+    "eslint --cache --cache-location tmp/cache/eslint/ --report-unused-disable-directives",
   "*.{ts,tsx,cts,mts}": () => "tsc",
   "*.{rb,rbi}": [() => "bin/srb typecheck", "bin/rubocop --force-exclusion"],
 };
