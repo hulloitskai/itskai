22039d8df2469900013fa13c43471aa290a25f07
Kai <hulloitskai@gmail.com>
2023-07-11 12:48:37 -0400

    Stop importing & analyzing Obsidian notes :(

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -14,21 +14,21 @@ Rails.application.configure do
         description: "Schedule purging of unattached ActiveStorage blobs.",
         cron: "0 */6 * * *",
       },
-      cleanup_obsidian_stubs: {
-        class: "CleanupObsidianStubJob",
-        description: "Destroy unreferenced stubs from Obsidian.",
-        cron: "15 */6 * * *",
-      },
-      analyze_obsidian_notes: {
-        class: "AnalyzeObsidianNotesJob",
-        description: "Analayze notes from Obsidian.",
-        cron: "30 */6 * * *",
-      },
-      import_obsidian_notes: {
-        class: "ImportObsidianNotesJob",
-        description: "Import notes from Obsidian.",
-        cron: "*/5 * * * *",
-      },
+      # cleanup_obsidian_stubs: {
+      #   class: "CleanupObsidianStubJob",
+      #   description: "Destroy unreferenced stubs from Obsidian.",
+      #   cron: "15 */6 * * *",
+      # },
+      # analyze_obsidian_notes: {
+      #   class: "AnalyzeObsidianNotesJob",
+      #   description: "Analayze notes from Obsidian.",
+      #   cron: "30 */6 * * *",
+      # },
+      # import_obsidian_notes: {
+      #   class: "ImportObsidianNotesJob",
+      #   description: "Import notes from Obsidian.",
+      #   cron: "*/5 * * * *",
+      # },
       import_journal_entries: {
         class: "ImportJournalEntriesJob",
         description: "Import journal entries from Notion.",
