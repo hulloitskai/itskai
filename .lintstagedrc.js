c7faaa22400889669db1ada6ac115e7e07066e91
Kai <hulloitskai@gmail.com>
2023-07-12 09:07:20 -0400

    Use Prettier cache

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 export default {
   "*.{js,mjs,cjs,jsx,ts,cts,mts,tsx,css,md,html,json,yaml,yml}":
-    "prettier --list-different",
+    "prettier --cache --list-different",
   "*.{js,mjs,cjs,jsx,ts,cts,mts,tsx,graphql}":
     "eslint --cache --cache-location tmp/cache/eslint/ --report-unused-disable-directives",
   "*.{ts,tsx,cts,mts}": () => "tsc",
