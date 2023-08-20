cb9216d5e2f3d140184e0e09dc982e4a9624e1d2
Kai <hulloitskai@gmail.com>
2023-08-20 01:48:46 -0400

    Add Google fonts to email template

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,7 +1,7 @@
 # typed: true
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/test_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
     user = Owner.current || User.first!
