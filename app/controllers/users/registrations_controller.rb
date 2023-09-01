390424c4a3681befe8e8b63afdda009d7faec4c1
Kai <hulloitskai@gmail.com>
2023-09-01 04:29:34 -0700

    Automatically perform Inertia redirect when required

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -25,14 +25,14 @@ module Users
         if resource.active_for_authentication?
           set_flash_message!(:notice, :signed_up)
           sign_up(resource_name, resource)
-          inertia_location(after_sign_up_path_for(resource))
+          redirect_to(after_sign_up_path_for(resource))
         else
           set_flash_message!(
             :notice,
             :"signed_up_but_#{resource.inactive_message}",
           )
           expire_data_after_sign_in!
-          inertia_location(after_inactive_sign_up_path_for(resource))
+          redirect_to(after_inactive_sign_up_path_for(resource))
         end
       else
         clean_up_passwords(resource)
