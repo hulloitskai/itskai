6c0c73db8605082b5407c4afa7274a1b617c6448
Kai <hulloitskai@gmail.com>
2023-09-03 18:13:09 -0700

    Refactor Telnyx client

--- app/services/telnyx_client.rb
+++ app/services/telnyx_client.rb
@@ -8,7 +8,8 @@ class TelnyxClient
 
   # == Configuration
   base_uri "https://api.telnyx.com/v2"
-  headers "Content-Type" => "application/json"
+  headers "Content-Type" => "application/json",
+          "Authorization" => proc { "Bearer #{Telnyx.api_key!}" }
 
   # == Current
   sig { returns(TelnyxClient) }
@@ -19,49 +20,33 @@ class TelnyxClient
     params(number: String, display_name: T.nilable(String)).returns(TelnyxCall)
   end
   def dial(number, display_name: nil)
-    response = self.class.post("/calls", request_options({
+    response = self.class.post("/calls", {
       body: {
         connection_id: Telnyx.app_id!,
         from: Telnyx.number!,
         from_display_name: display_name,
         to: number,
       }.to_json,
-    }))
+    })
     raise_response_errors(response)
     TelnyxCall.new(control_id: response.dig("data", "call_control_id"))
   end
 
   sig { params(call_control_id: String, message: String).void }
   def speak(call_control_id, message)
-    response = self.class.post(
-      "/calls/#{call_control_id}/actions/speak",
-      request_options({
-        body: {
-          payload: message,
-          voice: "female",
-          language: "en-US",
-        }.to_json,
-      }),
-    )
+    response = self.class.post("/calls/#{call_control_id}/actions/speak", {
+      body: {
+        payload: message,
+        voice: "female",
+        language: "en-US",
+      }.to_json,
+    })
     raise_response_errors(response)
   end
 
   private
 
   # == Helpers
-  sig { returns(T::Hash[Symbol, T.untyped]) }
-  def default_request_options
-    { headers: { "Authorization" => "Bearer #{Telnyx.api_key!}" } }
-  end
-
-  sig do
-    params(options: T::Hash[Symbol, T.untyped])
-      .returns(T::Hash[Symbol, T.untyped])
-  end
-  def request_options(options = {})
-    default_request_options.deep_merge(options)
-  end
-
   sig { params(response: T.untyped).void }
   def raise_response_errors(response)
     if (errors = response["errors"])
