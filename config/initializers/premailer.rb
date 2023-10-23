726d699427302c9d0a1056d60af6aff23c803471
Kai <hulloitskai@gmail.com>
2023-10-23 14:22:20 -0700

    Update README

--- config/initializers/premailer.rb
+++ config/initializers/premailer.rb
@@ -1,13 +1,9 @@
 # typed: strict
 # frozen_string_literal: true
 
-Rails.application.configure do
-  config.before_initialize do
-    url_options = Rails.application.routes.default_url_options.dup
-    url_options[:scheme] = url_options.delete(:protocol) || "http"
-    if url_options[:host].present?
-      url = Addressable::URI.new(url_options)
-      Premailer::Rails.config[:base_url] = url.to_s
-    end
-  end
+url_options = Rails.application.routes.default_url_options.dup
+url_options[:scheme] = url_options.delete(:protocol) || "http"
+if url_options[:host].present?
+  url = Addressable::URI.new(url_options)
+  Premailer::Rails.config[:base_url] = url.to_s
 end
