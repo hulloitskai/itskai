46798b0a1356a0084d9e03ffe80485f3e4f0dbc9
kai <hulloitskai@gmail.com>
2024-06-22 12:03:31 -0400

    Redirect /cathendant to / on cathy.earth

--- app/controllers/cathendants_controller.rb
+++ app/controllers/cathendants_controller.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 class CathendantsController < ApplicationController
+  # == Filters
+  before_action :redirect_to_canonical_url, only: :show
+
   # == Actions
   def show
     memos = CathendantMemo.chronological
@@ -12,5 +15,15 @@ class CathendantsController < ApplicationController
 
   def contribute
     render(inertia: "CathendantContributePage")
+  end
+
+  private
+
+  # == Helpers
+  sig { void }
+  def redirect_to_canonical_url
+    if request.hostname == "cathy.earth" && request.path != "/"
+      redirect_to(root_path)
+    end
   end
 end
