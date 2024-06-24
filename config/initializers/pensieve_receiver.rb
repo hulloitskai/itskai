833d73b2c7fee037664feb1eec2a21cc027563ee
kai <hulloitskai@gmail.com>
2024-06-24 10:57:23 -0400

    Disable Pensieve receiver

--- config/initializers/pensieve_receiver.rb
+++ config/initializers/pensieve_receiver.rb
@@ -1,6 +1,8 @@
 # typed: strict
 # frozen_string_literal: true
 
+return if ENV["PENSIEVE_RECEIVER_DISABLED"].truthy?
+
 Rails.application.configure do
   config.after_initialize do
     if PensieveReceiver.enabled?
