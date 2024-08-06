b433bfb46e92443e70b5f46adbc742b9cccb8c74
kai <hulloitskai@gmail.com>
2024-08-05 23:45:41 -0400

    Imagine using parentheses in assertions

--- test/system/render_test.rb
+++ test/system/render_test.rb
@@ -6,7 +6,7 @@ require "application_system_test_case"
 class RenderTest < ApplicationSystemTestCase
   test "renders" do
     visit(root_path)
-    assert_selector("#app")
-    assert_not_empty(find_by_id("app").find_all("*"))
+    assert_selector "#app"
+    assert_not_empty find_by_id("app").find_all("*")
   end
 end
