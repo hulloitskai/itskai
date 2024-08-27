5f3337e0e8e64a5f6c857fbb80dc419072346f68
kai <hulloitskai@gmail.com>
2024-08-27 16:48:47 -0400

    Fix error message formatting

--- app/helpers/errors.ts
+++ app/helpers/errors.ts
@@ -1,6 +1,6 @@
 export const formatError = ({ message }: Error): string => {
   message = message.charAt(0).toUpperCase() + message.slice(1);
-  if (!message.endsWith(".")) {
+  if (!!message && !message.endsWith(".")) {
     message += ".";
   }
   return message;
