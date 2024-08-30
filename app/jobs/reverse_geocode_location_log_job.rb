b62a5dcea0ef9bd40a0ee21932062e72962ee58d
kai <hulloitskai@gmail.com>
2024-08-30 14:33:06 -0400

    Time out reverse-geocoding jobs

--- app/jobs/reverse_geocode_location_log_job.rb
+++ app/jobs/reverse_geocode_location_log_job.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 class ReverseGeocodeLocationLogJob < ApplicationJob
+  # == Errors
+  class TimeoutError < StandardError; end
+
   # == Configuration
   good_job_control_concurrency_with(
     key: -> {
@@ -11,12 +14,15 @@ class ReverseGeocodeLocationLogJob < ApplicationJob
     },
     perform_limit: 1,
   )
-  retry_on Geocoder::OverQueryLimitError
+  retry_on Geocoder::OverQueryLimitError, TimeoutError,
+           wait: :polynomially_longer
   queue_with_priority 10
 
   # == Job
   sig { params(log: LocationLog).void }
   def perform(log)
-    log.reverse_geocode
+    Timeout.timeout(10, TimeoutError) do
+      log.reverse_geocode
+    end
   end
 end
