38f8cf7dac21dc75bb92f0a79225e7feffdea6d9
kai <hulloitskai@gmail.com>
2024-08-29 22:40:15 -0400

    Raise geocoding errors

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,7 @@
 # typed: strict
 # frozen_string_literal: true
 
-Geocoder.configure(
-  logger: Rails.logger,
-  always_raise: [ Geocoder::InvalidRequest ],
-)
+Geocoder.configure(logger: Rails.logger, always_raise: :all)
 
 # == Here
 Geocoder.configure(lookup: :here, api_key: ENV["HERE_API_KEY"])
