dbb4bbe1c3ec14e547c8102dfa751f22ac221c9f
kai <hulloitskai@gmail.com>
2024-09-07 10:02:56 -0400

    Serializer generator trims 'Serializer' suffixes

--- lib/generators/serializer_generator.rb
+++ lib/generators/serializer_generator.rb
@@ -2,15 +2,20 @@
 # frozen_string_literal: true
 
 class SerializerGenerator < Rails::Generators::NamedBase
+  extend T::Sig
+
+  sig { void }
   def copy_serializer_file
     template("serializer.rb", "app/serializers/#{serializer_file_name}")
   end
 
+  sig { returns(String) }
   def serializer_class_name
-    class_name + "Serializer"
+    class_name.delete_suffix("Serializer") + "Serializer"
   end
 
+  sig { returns(String) }
   def serializer_file_name
-    file_name + "_serializer.rb"
+    file_name.delete_suffix("_serializer") + "_serializer.rb"
   end
 end
