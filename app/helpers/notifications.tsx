c35b453d61bd0af9da091c887aa29fa9e526b287
kai <hulloitskai@gmail.com>
2024-09-30 10:21:46 -0400

    Nits

--- app/helpers/notifications.tsx
+++ app/helpers/notifications.tsx
@@ -4,7 +4,7 @@ import { showNotification } from "@mantine/notifications";
 import ExclamationTriangleIcon from "~icons/heroicons/exclamation-triangle-20-solid";
 import InformationCircleIcon from "~icons/heroicons/information-circle-20-solid";
 
-export const showAlert = (props: NotificationData) => {
+export const showAlert = (props: NotificationData): void => {
   showNotification({
     color: "red",
     icon: <ExclamationTriangleIcon />,
@@ -12,7 +12,7 @@ export const showAlert = (props: NotificationData) => {
   });
 };
 
-export const showNotice = (props: NotificationData) => {
+export const showNotice = (props: NotificationData): void => {
   showNotification({
     color: "primary",
     icon: <InformationCircleIcon />,
@@ -20,7 +20,7 @@ export const showNotice = (props: NotificationData) => {
   });
 };
 
-export const showSuccessNotice = (props: NotificationData) => {
+export const showSuccessNotice = (props: NotificationData): void => {
   showNotification({
     color: "green",
     icon: <SuccessIcon />,
@@ -36,10 +36,10 @@ export interface ShowChangesSavedNoticeProps
 export const showChangesSavedNotice = ({
   to,
   ...otherProps
-}: ShowChangesSavedNoticeProps) => {
+}: ShowChangesSavedNoticeProps): void => {
   showSuccessNotice({
     title: "Changes saved",
-    message: `${humanize(to)} was updated.`,
+    message: `Updated ${to}.`,
     ...otherProps,
   });
 };
