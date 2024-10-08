2859e3ec1575d589b332e7c9fa16d83272f44cf5
kai <hulloitskai@gmail.com>
2024-10-08 14:19:59 -0400

    Update Admin module

--- lib/admin.rb
+++ lib/admin.rb
@@ -7,35 +7,30 @@ module Admin
   extend T::Sig
 
   # == Accessors
-  sig { returns(T.nilable(String)) }
-  def self.emails_expr
-    credentials.emails
-  end
-
   sig { returns(T::Array[String]) }
   def self.emails
-    @emails ||= T.let(
-      if (expr = emails_expr)
-        expr.split(",").select do |entry|
-          entry.include?("@") && EmailValidator.valid?(entry)
-        end
-      else
-        []
-      end,
-      T.nilable(T::Array[String]),
-    )
+    value = credentials.emails or return []
+    case value
+    when String
+      [value]
+    when Array
+      value
+    else
+      raise "Invalid admin emails credential value: #{value.inspect}"
+    end
   end
 
   sig { returns(T::Array[String]) }
   def self.email_domains
-    @email_domains ||= T.let(
-      if (expr = emails_expr)
-        expr.split(",").select { |entry| entry.exclude?("@") }
-      else
-        []
-      end,
-      T.nilable(T::Array[String]),
-    )
+    value = credentials.email_domains or return []
+    case value
+    when String
+      [value]
+    when Array
+      value
+    else
+      raise "Invalid admin email domains credential value: #{value.inspect}"
+    end
   end
 
   # == Helpers
