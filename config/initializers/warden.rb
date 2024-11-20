773c8b36fee6c2ba4db4544187fc0f599a86ad83
kai <hulloitskai@gmail.com>
2024-11-20 12:00:51 -0500

    Add authentication breadcrumb

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,12 +1,19 @@
 # typed: strict
 # frozen_string_literal: true
 
-Warden::Manager.after_set_user do |user, auth, opts|
-  scope = opts.fetch(:scope)
-  auth.cookies.signed["#{scope}.id"] = user.id
+Warden::Manager.after_set_user do |user|
+  user = T.let(user, User)
+  crumb = Sentry::Breadcrumb.new(
+    category: "auth",
+    message: "Authenticated user #{user.id} (#{user.email})",
+    level: "info",
+  )
+  Sentry.add_breadcrumb(crumb)
+  # scope = opts.fetch(:scope)
+  # auth.cookies.signed["#{scope}.id"] = user.id
 end
 
-Warden::Manager.before_logout do |_, auth, opts|
-  scope = opts.fetch(:scope)
-  auth.cookies.signed["#{scope}.id"] = nil
-end
+# Warden::Manager.before_logout do |_, auth, opts|
+#   scope = opts.fetch(:scope)
+#   auth.cookies.signed["#{scope}.id"] = nil
+# end
