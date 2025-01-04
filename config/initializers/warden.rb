0cbed46ba039758d3e2f938d62d11686bd3cd8f2
kai <hulloitskai@gmail.com>
2025-01-04 12:02:27 -0500

    Use secure remember_me cookies

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,4 +1,4 @@
-# typed: strict
+# typed: true
 # frozen_string_literal: true
 
 Warden::Manager.after_set_user do |user|
@@ -9,11 +9,4 @@ Warden::Manager.after_set_user do |user|
     level: "info",
   )
   Sentry.add_breadcrumb(crumb)
-  # scope = opts.fetch(:scope)
-  # auth.cookies.signed["#{scope}.id"] = user.id
 end
-
-# Warden::Manager.before_logout do |_, auth, opts|
-#   scope = opts.fetch(:scope)
-#   auth.cookies.signed["#{scope}.id"] = nil
-# end
