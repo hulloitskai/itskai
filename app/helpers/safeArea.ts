a1efe1a068a8a7e85a511b3e3800e4682b35648e
kai <hulloitskai@gmail.com>
2025-01-16 10:10:47 -0500

    Refactoring is my passion

--- app/helpers/safeArea.ts
+++ app/helpers/safeArea.ts
@@ -16,32 +16,41 @@ export const useSafeViewportRect = (): Rect | undefined => {
   );
   const viewport = useViewportSize();
   useEffect(() => {
+    // Only detect safe area when in mobile standalone mode.
     if (!mobileStandalone) {
       setRect(undefined);
       return;
     }
 
-    const documentStyle = getComputedStyle(document.documentElement);
-    const [leftInset, topInset, rightInset, bottomInset] = [
-      "left",
-      "top",
-      "right",
-      "bottom",
-    ].map(side =>
-      getPixels(documentStyle.getPropertyValue(`--safe-area-inset-${side}`)),
-    ) as [number, number, number, number];
-    if (!leftInset && !topInset && !rightInset && !bottomInset) {
+    // If all insets are 0, keep the safe area viewport undefined.
+    const insets = getSafeAreaInsets();
+    if (!Object.values(insets).some(Boolean)) {
       setRect(undefined);
       return;
     }
 
-    const x = leftInset;
-    const y = topInset;
-    const width = viewport.width - leftInset - rightInset;
-    const height = viewport.height - topInset - bottomInset;
+    const x = insets.left;
+    const y = insets.top;
+    const width = viewport.width - insets.left - insets.right;
+    const height = viewport.height - insets.top - insets.bottom;
     setRect({ x, y, width, height });
   }, [mobileStandalone, viewport]);
   return rect;
+};
+
+interface Insets {
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+}
+
+const getSafeAreaInsets = (): Insets => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const initialInsets = { left: 0, top: 0, right: 0, bottom: 0 };
+  return mapValues(initialInsets, (_, side) =>
+    getPixels(documentStyle.getPropertyValue(`--safe-area-inset-${side}`)),
+  );
 };
 
 const getPixels = (value: string): number =>
