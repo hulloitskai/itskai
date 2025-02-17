ba8f6e8cd1840eabae62454dab6cda0ac38588d8
kai <hulloitskai@gmail.com>
2025-02-17 15:41:56 -0500

    Reset login form on sign-in failure

--- lib/custom_devise_failure_app.rb
+++ lib/custom_devise_failure_app.rb
@@ -4,18 +4,6 @@
 class CustomDeviseFailureApp < Devise::FailureApp
   extend T::Sig
 
-  def respond
-    if http_auth?
-      http_auth
-    elsif request.content_type == "application/json"
-      self.status = 401
-      self.content_type = "application/json"
-      self.response_body = { error: i18n_message(:invalid) }.to_json
-    else
-      redirect
-    end
-  end
-
   sig { params(options: Hash).returns(Hash) }
   def i18n_options(options)
     options.merge(
